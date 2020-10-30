091d4ef79598978795076e4605aefa4a0f63ee73
Blake Nelson <bnelson95@gmail.com>
2020-10-29 22:26:53 -0400

    rename

---
+++ models/item.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose'
+
+const ItemSchema = new mongoose.Schema({
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  createdBy: {
+    type: String
+  }
+})
+
+export default mongoose.model('item', ItemSchema)
