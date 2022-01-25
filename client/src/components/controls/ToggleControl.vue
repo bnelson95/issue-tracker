0fe2847bd2e2e47ee891b91dbd9a48aab693a868
Blake Nelson <bnelson95@gmail.com>
2022-01-24 21:19:59 -0500

    Create ToggleControl.vue

---
+++ client/src/components/controls/ToggleControl.vue
@@ -0,0 +1,50 @@
+<template>
+  <control-wrapper :title="title">
+    <template v-slot:control-content>
+      <b-form-radio-group
+        id="toggle-control-radios"
+        :checked="value"
+        :options="options"
+        name="toggle-control-radios"
+        buttons
+        button-variant="light"
+        class="d-flex"
+        @input="$emit('input', $event)">
+      </b-form-radio-group>
+    </template>
+  </control-wrapper>
+</template>
+
+<script>
+import ControlWrapper from '@/components/controls/ControlWrapper.vue'
+export default {
+  name: 'ToggleControl',
+  components: {
+    ControlWrapper
+  },
+  data () {
+    return {
+      options: [
+        { text: 'Yes', value: true },
+        { text: 'No', value: false },
+      ]
+    }
+  },
+  model: {
+    prop: 'value',
+    event: 'input'
+  },
+  props: {
+    title: {
+      type: String
+    },
+    value: {
+      type: Boolean
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
